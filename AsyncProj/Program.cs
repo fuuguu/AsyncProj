fbcc75198aa42d9fab2dbdac52022dfd9fcaac37
linvyy <linvyy@DESKTOP-39MTSN8>
2023-06-28 04:42:54 +0200

    Добавьте файлы проекта.

---
+++ AsyncProj/Program.cs
@@ -0,0 +1,13 @@
+﻿Random r = new Random();
+int[] mas = new int[30];
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = r.Next(-9, 10);
+    Console.Write(mas[i] + " ");
+}
+
+Console.WriteLine();
+
+int sum = mas.Where(x => x < 0 && x % 2 != 0).Sum();
+
+Console.WriteLine($"Сумма элементов, которые являются нечетными и отрицательными: {sum}");
